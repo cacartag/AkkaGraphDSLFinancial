7001d25a3d2a80194b34f594c9067e07714608dc
christopher <christophercartagena40@gmail.com>
2024-08-25 15:12:15 -0500

    added test and modified stream

--- src/main/AuthGraphDSL.java
+++ src/main/AuthGraphDSL.java
@@ -1,3 +1,4 @@
+import akka.Done;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.stream.ClosedShape;
@@ -8,10 +9,12 @@ import akka.stream.javadsl.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletionStage;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
+import jdk.jshell.JShell;
 import models.*;
 
 public class AuthGraphDSL {
@@ -20,30 +23,51 @@ public class AuthGraphDSL {
     }
 
 //    List<Transaction>
-    void getTransactionFromAuth(Auth auth, List<Tender> tenders, List<Settlement> settlements, ActorSystem system){
+    RunnableGraph<CompletionStage<Set<String>>> getTransactionFromAuth(Auth auth, List<Tender> tenders, List<Settlement> settlements, ActorSystem system){
+
+        final Sink<String, CompletionStage<Set<String>>> allSink = Sink.collect(Collectors.toSet());
+        final RunnableGraph<CompletionStage<Set<String>>> result =
         RunnableGraph.fromGraph(
                 GraphDSL.create(
-                        Sink.ignore(),
+                        allSink,
                         (builder,out) -> {
+
                             final Outlet<String> source = builder.add(Source.single(auth.paymentId())).out();
+
                             final UniformFanOutShape<String, String> paymentIdBCast = builder.add(Broadcast.create(2));
 
                             final Flow<String, String, NotUsed> tenderInitial =
-                                    tenderRetrieveFlow(tenders, (Tender tender, String key) -> {return tender.paymentId() == key;}, system)
+                                    tenderRetrieveFlow(tenders, (Tender tender, String key) -> {
+                                        System.out.println("checking tender: " + tender.paymentId() + " key: " + key);
+                                        return tender.paymentId() == key;}, system)
                                             .map(x -> x.clientMatcher());
 
                             final Flow<String, String, NotUsed> settlementInitial =
-                                    settlementRetrieveFlow(settlements, (Settlement settlement, String key) -> {return settlement.paymentId() == key;}, system)
+                                    settlementRetrieveFlow(settlements, (Settlement settlement, String key) -> settlement.paymentId() == key, system)
                                             .map(x -> x.clientMatcher());
 
+                            final UniformFanInShape<String, String> merge = builder.add(Merge.create(2));
+
                             builder.from(source).viaFanOut(paymentIdBCast);
-                            builder.from(paymentIdBCast.out(0)).via(builder.add(tenderInitial));
-                            builder.from(paymentIdBCast.out(1)).via(builder.add(settlementInitial));
+
+                            builder.from(paymentIdBCast.out(0)).via(builder.add(tenderInitial)).toInlet(merge.in(0));
+                            builder.from(paymentIdBCast.out(1)).via(builder.add(settlementInitial)).toInlet(merge.in(1));
+
+                            builder.from(merge).via(builder.add(
+                                    Flow.of(String.class).map(x -> {
+                                        System.out.println("Received: " + x);
+                                        return x;}
+                                    )
+                            )).to(out);
+
+//                            builder.from(merge).to(out);
 
                             return ClosedShape.getInstance();
                         }
                 )
         );
+
+        return result;
 
 //        Source.single(auth.paymentId())
 //                .via(tenderRetrieveFlow(tenders, (tender, key) -> {return tender.paymentId() == key;}, system));
@@ -55,6 +79,9 @@ public class AuthGraphDSL {
                 {
                     CompletionStage<List<Tender>> getMatchingTenders =  Source
                             .from(tenders)
+                            .map(x -> {
+                                System.out.println("Running from inside tender");
+                                return x;})
                             .filter(tender -> bi.apply(tender, key))
                             .runWith(Sink.collect(Collectors.toList()), system);
                     return Source.from(getMatchingTenders.toCompletableFuture().get());
--- src/test/java/AuthGraphDSLTest.java
+++ src/test/java/AuthGraphDSLTest.java
@@ -1,15 +1,63 @@
+import akka.Done;
+import akka.actor.ActorSystem;
+import akka.stream.javadsl.RunnableGraph;
+import akka.stream.javadsl.Sink;
+import akka.testkit.javadsl.TestKit;
+import models.*;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static junit.framework.Assert.assertEquals;
 
 public class AuthGraphDSLTest {
 
     @Test
-    public void runAuthGraph(){
+    public void runAuthGraph() throws Exception {
 
         AuthGraphDSL testGraph = new AuthGraphDSL();
 
+        ActorSystem system = ActorSystem.create("testActorSystem");
+
+        Auth testAuth = new Auth("paymentId1", "Auth1");
+
+        Tender testTender1 = new Tender("paymentId1", "clientMatcher1",
+                "invoiceId1", "tenderPayload1");
+        Settlement testSettlement1 = new Settlement("paymentId1", "invoiceId1",
+                "clientMatcher1", "settlementPayload1");
+
+        List<Tender> tenderList = new ArrayList();
+        tenderList.add(testTender1);
+
+        List<Settlement> settlementList = new ArrayList();
+        settlementList.add(testSettlement1);
 
 
-        System.out.println("Running from tests!!!");
+        Sink<Object, CompletionStage<Done>> sink1 = Sink.ignore();
+        Sink<String, CompletionStage<List<String>>> sink2 = Sink.collect(Collectors.toList());
+
+
+
+        final TestKit probe = new TestKit(system);
+
+        RunnableGraph<CompletionStage<Set<String>>> authGraph = testGraph.getTransactionFromAuth(testAuth, tenderList, settlementList, system);
+
+        CompletionStage<Set<String>> running = authGraph.run(system);
+
+        final Set<String> result = running.toCompletableFuture().get(3, TimeUnit.SECONDS);
+
+        List<String> expect = new ArrayList();
+        expect.add("clientMatcher1");
+
+        assertEquals(expect, result.stream().toList());
+//        Thread.sleep(4000);
+//        System.out.println("Running from tests!!!");
 
     }
 
